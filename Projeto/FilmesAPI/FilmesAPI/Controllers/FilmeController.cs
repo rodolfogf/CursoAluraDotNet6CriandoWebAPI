9c283215725cf4ec58ba6418d1ae8b0d03ad4dac
Rodolfo Faria <rodolfogf@gmail.com>
2023-03-09 22:30:38 -0300

    Aula 2 - Enviando requisições para a API

---
+++ Projeto/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo );
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.Duracao);
+    }
+}
---
+++ Projeto/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmesAPI.Models;
+
+public class Filme
+{
+     public String Titulo { get; set; }
+    public string Genero { get; set; }
+    public int Duracao { get; set; }
+
+}
