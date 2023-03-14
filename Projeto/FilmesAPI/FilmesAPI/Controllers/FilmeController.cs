bc1df34e3109d0c2ded9d7e7a894244928d57e93
Rodolfo Faria <rodolfogf@gmail.com>
2023-03-13 23:51:55 -0300

    Aula 03 - Recuperando filmes por id

--- Projeto/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
+++ Projeto/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -8,19 +8,27 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionarFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.Write($"{filme.Titulo} | ");
         Console.Write($"{filme.Genero} | ");
-        Console.Write($"{filme.Duracao} min"); 
+        Console.WriteLine($"{filme.Duracao} min"); 
     }
 
-    [HttpPost]
+    [HttpGet]
     public IEnumerable<Filme> RecuperaFilmes()
     {
         return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperarFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }
--- Projeto/FilmesAPI/FilmesAPI/Models/Filme.cs
+++ Projeto/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -4,6 +4,7 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
     [Required(ErrorMessage = "O título do filme é obrigatório")]
     public String Titulo { get; set; }
     [Required(ErrorMessage = "O gênero do filme é obrigatório")]
@@ -12,5 +13,4 @@ public class Filme
     [Required]
     [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 300 minutos")]
     public int Duracao { get; set; }
-
 }
